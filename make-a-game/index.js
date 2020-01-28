386c0c683770a8b506c0779325bb7d41160dafc4
liz andry <mxlizandry@gmail.com>
2020-01-28 11:43:33 -0800

    made dice an object

--- make-a-game/index.js
+++ make-a-game/index.js
@@ -1,20 +1,36 @@
-const die0 = ["A", "A", "E", "E", "G", "N"];
-const die1 = ["A", "B", "B", "J", "O", "O"];
-const die2 = ["A", "C", "H", "O", "P", "S"];
-const die3 = ["A", "F", "F", "K", "P", "S"];
-const die4 = ["A", "O", "O", "T", "T", "W"];
-const die5 = ["C", "I", "M", "O", "T", "U"];
-const die6 = ["D", "E", "I", "L", "R", "X"];
-const die7 = ["D", "E", "L", "R", "V", "Y"];
-const die8 = ["D", "I", "S", "T", "T", "Y"];
-const die9 = ["E", "E", "G", "H", "N", "W"];
-const die10 = ["E", "E", "I", "N", "S", "U"];
-const die11 = ["E", "H", "R", "T", "V", "W"];
-const die12 = ["E", "I", "O", "S", "S", "T"];
-const die13 = ["E", "L", "R", "T", "T", "Y"];
-const die14 = ["H", "I", "M", "N", "U", "Qu"];
-const die15 = ["H", "L", "N", "N", "R", "Z"];
-//remember to allow for two letters in "Qu"
+document.querySelector('.box');
+
+
+
+
+
+
+
+
+
+
+
+
+
+const dice = {
+    'letter': ["A", "A", "E", "E", "G", "N"],
+    'letter': ["A", "B", "B", "J", "O", "O"],
+    'letter': ["A", "C", "H", "O", "P", "S"],
+    'letter': ["A", "F", "F", "K", "P", "S"],
+    'letter': ["A", "O", "O", "T", "T", "W"],
+    'letter': ["C", "I", "M", "O", "T", "U"],
+    'letter': ["D", "E", "I", "L", "R", "X"],
+    'letter': ["D", "E", "L", "R", "V", "Y"],
+    'letter': ["D", "I", "S", "T", "T", "Y"],
+    'letter': ["E", "E", "G", "H", "N", "W"],
+    'letter': ["E", "E", "I", "N", "S", "U"],
+    'letter': ["E", "H", "R", "T", "V", "W"],
+    'letter': ["E", "I", "O", "S", "S", "T"],
+    'letter': ["E", "L", "R", "T", "T", "Y"],
+    'letter': ["H", "I", "M", "N", "U", "Qu"],
+    'letter': ["H", "L", "N", "N", "R", "Z"],
+    //remember to allow for two letters in "Qu"
+}
 
 const letters = Array.fromCharCode(65 + 90);
 //this is nothing, i tried to bind an array of letters by using unicode references
