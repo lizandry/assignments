37bf58bee36066e6d12efa973e1ebae16228109d
liz andry <mxlizandry@gmail.com>
2020-01-27 17:05:52 -0800

    hand-coded the boggle dice because it seemed easier

---
+++ make-a-game/src/components/Dice/index.jsx
@@ -0,0 +1,23 @@
+const die0 = ["A", "A", "E", "E", "G", "N"];
+const die1 = ["A", "B", "B", "J", "O", "O"];
+const die2 = ["A", "C", "H", "O", "P", "S"];
+const die3 = ["A", "F", "F", "K", "P", "S"];
+const die4 = ["A", "O", "O", "T", "T", "W"];
+const die5 = ["C", "I", "M", "O", "T", "U"];
+const die6 = ["D", "E", "I", "L", "R", "X"];
+const die7 = ["D", "E", "L", "R", "V", "Y"];
+const die8 = ["D", "I", "S", "T", "T", "Y"];
+const die9 = ["E", "E", "G", "H", "N", "W"];
+const die10 = ["E", "E", "I", "N", "S", "U"];
+const die11 = ["E", "H", "R", "T", "V", "W"];
+const die12 = ["E", "I", "O", "S", "S", "T"];
+const die13 = ["E", "L", "R", "T", "T", "Y"];
+const die14 = ["H", "I", "M", "N", "U", "Qu"];
+const die15 = ["H", "L", "N", "N", "R", "Z"];
+//remember to allow for two letters in "Qu"
+
+
+// EGINTV	EIOSST
+// EHINPS	ELRTTY
+// ELPSTU	HIMNUQu
+// GILRUW	HLNNRZ
