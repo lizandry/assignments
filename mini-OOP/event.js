009e62cdd1bbc5f355c0d24b1c03450cf02bde6a
liz andry <mxlizandry@gmail.com>
2020-01-31 16:09:09 -0800

    mini oop

--- mini-OOP/event.js
+++ mini-OOP/event.js
@@ -1,3 +1,12 @@
+//heads up!
+//i've been using eventObj1 to test all of my functions
+//and didn't realize it until literally 3:40
+//i THINK my logic is solid, but it's working on the wrong part of a given class object
+//ie, the visible addAvailableTickets function works for eventArray.addAvailableTickets
+//the commented-out addAvailableTickets function works for eventObj1.addAvailableTickets
+
+//i'm going to take a break from it for the rest of the day, and go through and fix my oopsie this weekend
+
 class Event {
     constructor(name, description) {
         this.name = name;
@@ -7,22 +16,56 @@ class Event {
     addAvailableTickets(TicketType) {
         this.availableTickets.push(TicketType);
     }
+
+    // addAvailableTickets(ticketArr) {
+
+
+    //     for (let i in ticketArr) {
+    //         this.availableTickets.push(ticketArr[i]);
+    //     }
+
+    // }
     allTickets() {
         let ticketStr = 'all tickets: ';
+        let count = 1;
         for (let i in this.availableTickets) {
-            ticketStr = ticketStr.concat((i + 1) + '. ' + this.availableTickets[i].seating + ' (' + this.availableTickets[i].price + ') ');
+            ticketStr = ticketStr.concat((count) + '. ' + this.availableTickets[i].seating + ' (' + this.availableTickets[i].price + ') ');
+            count++;
         }
         return ticketStr;
     }
-    searchTickets(upper, lower) {
-        let eligible = 'eligible tickets: '
-        for (let i in this.availableTickets) {
-            console.log(this.availableTickets[i].seating)
 
-            ticketStr = ticketStr.concat((i + 1) + '. ' + this.availableTickets[i].seating + ' (' + this.availableTickets[i].price + ') ');
+    searchTickets(lower, upper) {
+        let eligible = 'eligible tickets: ';
+        let count = 1;
+
+        if (!lower && !upper) {
+            for (let i in this.availableTickets) {
+                eligible = eligible.concat((count) + '. ' + this.availableTickets[i].seating + ' (' + this.availableTickets[i].price + ') ');
+                count++;
+            }
+
+        } else if (!upper) {
+            for (let i in this.availableTickets) {
+                if (this.availableTickets[i].price < lower) {
+                    eligible = eligible.concat((count) + '. ' + this.availableTickets[i].seating + ' (' + this.availableTickets[i].price + ') ');
+                    count++;
+                }
+            }
+        } else
+            for (let i in this.availableTickets) {
+                if (this.availableTickets[i].price > lower && this.availableTickets[i].price < upper) {
+                    eligible = eligible.concat((count) + '. ' + this.availableTickets[i].seating + ' (' + this.availableTickets[i].price + ') ');
+                    count++;
+                }
+            }
+        if (eligible === 'eligible tickets: ') {
+            eligible = 'No Tickets Available';
         }
+        return eligible;
     }
 }
+
 
 class TicketType {
     constructor(seating, price) {
@@ -38,42 +81,51 @@ const eventObj3 = new Event('Let\'s Go Atsuko: A (Woke) Japanese Game Show', 'ho
 const eventObj4 = new Event('City vs City: San Francisco vs Los Angeles', 'with Emily Russell and guest judges Solange Castro and Tommy Whalen, featuring Ben Kolina & Alexandria Love (SF) and Hannah Einbinder & Geoff Plitt (LA)');
 const eventObj5 = new Event('Trump vs Bernie', 'with Anthony Atamanuik & James Adomian');
 const eventObj6 = new Event('Nightlife at the California Academy of Sciences', 'you get to drink in a museum, and comedians will be there');
-
-const eventArray = new Array();
-
+let eventArray = new Array();
 eventArray.push(eventObj1);
 eventArray.push(eventObj2, eventObj3, eventObj4, eventObj5, eventObj6);
 
-
 const tick1a = new TicketType('balcony', 40)
 const tick1b = new TicketType('orchestra', 120)
+const tick1c = new TicketType('peanut gallery', 20)
+const tick1d = new TicketType('outside', 10)
+const tick1e = new TicketType('general admission', 65)
+const tick1f = new TicketType('at home', -10)
 const tick2 = new TicketType('table seating', 35)
 const tick3 = new TicketType('general', 25)
 const tick4 = new TicketType('general', 20)
 const tick5 = new TicketType('table seating', 35)
 const tick6 = new TicketType('ticket', 12)
+let ticketTypeArray = new Array();
+ticketTypeArray.push(tick1a, tick1b, tick1c, tick1d, tick1e, tick1f, tick2, tick3, tick4, tick5, tick6);
+// eventArray.addAvailableTickets(ticketTypeArray)
+// console.log(eventArray.availableTickets)
 
-
-eventObj1.addAvailableTickets(tick1a);
-eventObj1.addAvailableTickets(tick1b);
-eventObj2.addAvailableTickets(tick2)
-eventObj3.addAvailableTickets(tick3)
-eventObj4.addAvailableTickets(tick4)
-eventObj5.addAvailableTickets(tick5)
-eventObj6.addAvailableTickets(tick6)
+//i'm numberwanging it
 
 
 
-console.log(eventObj1.allTickets())
 
+// console.log(eventObj1.allTickets())
+// console.log(eventObj1.searchTickets());
+// console.log(eventObj1.searchTickets(35));
+// console.log(eventObj1.searchTickets(50, 120));
+// console.log(eventObj1.searchTickets(-15, 50));
+//console.log(eventObj1.searchTickets(25, 100));
+//console.log(eventObj1.searchTickets(0, 39))
+//console.log(eventObj1.searchTickets(230, 300))
 
-
-// console.log(eventObj2.availableTickets);
-// console.log(eventObj1.availableTickets);
-// console.log(eventObj1.availableTickets[0])
 // $(document).ready(function() {
 //     let html = '';
 //     $.each(eventArray, function(index, item) {
+//         html += `<li>${item.name} - ${item.description}</li>`;
+//     });
+//     // insert final html into #event...
+//     $('#event').html(html);
+// });
+// $(document).ready(function() {
+//     let html = '';
+//     $.each(eventArray, searchTickets(-20, 20) {
 //         html += `<li>${item.name} - ${item.description}</li>`;
 //     });
 //     // insert final html into #event...
