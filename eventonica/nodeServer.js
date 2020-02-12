402280510eb15d5c45139a8aa7040b7fa6466496
liz andry <mxlizandry@gmail.com>
2020-02-11 16:44:14 -0800

    code borrowed from amy and zarina

--- eventonica/nodeServer.js
+++ eventonica/nodeServer.js
@@ -19,3 +19,18 @@ server.listen(port, (err) => {
 
     console.log(`server is listening on ${port}`)
 })
+
+// let http = require("http");
+// let fs = require("fs");
+// let server = http.createServer(function(req, res){
+//     console.log("request was made: " + req.url);
+//     if(req.url === "/home" || req.url === "/"){
+//         res.writeHead(200, {"Content-Type": "text/html"});
+//         fs.createReadStream(__Eventonica + "/index.html").pipe(res);
+//     } else if(req.url === "/contact"){
+//         res.writeHead(200, {"Content-Type": "text/html"});
+//         fs.createReadStream(__Eventonica + "/contact.html").pipe(res);
+//     }
+// });
+// server.listen(3000, "127.0.0.1");
+// console.log("Listening to port 3000");
