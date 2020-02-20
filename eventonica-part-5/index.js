019385ac4289cb2d8c2fe7659e3709c045f9dec5
liz andry <mxlizandry@gmail.com>
2020-02-20 12:30:38 -0800

    restored index.js express doc

---
+++ eventonica-part-5/index.js
@@ -0,0 +1,36 @@
+const { EventRecommender, User, Event } = require('./EventRecommender.js');
+const express = require('express');
+const path = require('path');
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json())
+app.use(express.static(__dirname + "/"));
+const website = new EventRecommender();
+
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/events', (req, res) => {
+    console.log(website.events);
+    res.send(website.events);
+});
+
+app.get('/event/:eventId', (req, res) => {
+    console.log(req.params.eventId);
+    //res.send(TODO);
+});
+
+app.post('/events', (req, res) => {
+    console.log(req.body.title);
+    res.send(website.addEvent(req.body));
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
